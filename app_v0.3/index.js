c2a2d81dec0e015b0ba79e36f8ccf0ec77ba29c3
Alonzo Wahl <aawahl@gmail.com>
2016-11-02 17:49:32 -0400

    got basic fcm working on android

--- app_v0.3/index.js
+++ app_v0.3/index.js
@@ -2,7 +2,7 @@
 import React,{ Component } from 'react'
 import { View, StyleSheet, StatusBar }from 'react-native'
 import { Provider }        from 'react-redux'
-
+import FCM from 'react-native-fcm'
 
 import Store from './createStore'
 import Router from './router.js'
@@ -22,6 +22,45 @@ const navigationContext = new NavigationContext({
 
 
 export default class App extends Component {
+
+  componentDidMount() {
+    FCM.getFCMToken().then(token => {
+        FCM.subscribeToTopic('/topics/demo')
+        console.log(token)
+        // store fcm token in your server
+    });
+    this.notificationUnsubscribe = FCM.on('notification', (notif) => {
+        // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+        console.log(notif);
+        if(notif.local_notification){
+          return;
+        }
+        if(notif.opened_from_tray){
+          //app is open/resumed because user clicked banner
+        }
+
+        FCM.presentLocalNotification({
+          id: notif.id,
+          title: notif.title,
+          body: notif.body,
+          priority: "high",
+          click_action: notif.click_action,
+          show_in_foreground: true,
+        })
+
+    });
+    this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
+        console.log(token)
+        // fcm token may not be available on first load, catch it here
+    });
+  }
+
+  componentWillUnmount() {
+      // prevent leaking
+      this.refreshUnsubscribe();
+      this.notificationUnsubscribe();
+  }
+
   render () {
     return (
       // <Login />
