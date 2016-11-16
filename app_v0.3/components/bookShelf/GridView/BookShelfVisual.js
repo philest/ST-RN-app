8a224a4309a4bfa782eaaa58f3069e1e44239d19
Alonzo Wahl <aawahl@gmail.com>
2016-11-16 10:17:10 -0500

    better shelves

--- app_v0.3/components/bookShelf/GridView/BookShelfVisual.js
+++ app_v0.3/components/bookShelf/GridView/BookShelfVisual.js
@@ -8,10 +8,12 @@ import {
   Display
 } from 'react-native'
 
-const SHELF_THICKNESS=20
-const STUB_Y= 15
-const STUB_X= 20
-const STUB_OFFSET = 12
+import { SPACE_BETWEEN_SHELVES } from './GridItem'
+
+const SHELF_THICKNESS=15
+const STUB_Y= 10
+const STUB_X= 15
+const STUB_OFFSET = 10
 
 const BROWN = '#80461B'
 const DARK_BROWN = '#6B4423'
@@ -22,9 +24,9 @@ export default class BookShelfVisual extends Component {
   render () {
     const width = this.props.width
     return (
-      <View style={[styles.container, {flex:1, height:SHELF_THICKNESS-STUB_Y+STUB_OFFSET}]}>
+      <View style={[styles.container, {height:SHELF_THICKNESS-STUB_Y+STUB_OFFSET}]}>
         <View style={{flexDirection:'column'}}>
-          <View style={[styles.shelfBase1, {flex:1, height:SHELF_THICKNESS/2}]}/>
+          <View style={[styles.shelfBase1, {flex:1, height:SHELF_THICKNESS/4}]}/>
           <View style={[styles.shelfBase2, {flex:1, height:SHELF_THICKNESS/2}]}/>
         </View>
         <Knob style={{position:'absolute', top:STUB_OFFSET, left:20}}/>
@@ -38,7 +40,7 @@ styles = StyleSheet.create({
   container: {
     position:'absolute',
     left:20,
-    bottom:40,
+    bottom:SPACE_BETWEEN_SHELVES - SHELF_THICKNESS/2,
   },
   shelfBase1: {
     backgroundColor: BROWN,
--- app_v0.3/components/bookShelf/GridView/GridItem.js
+++ app_v0.3/components/bookShelf/GridView/GridItem.js
@@ -3,6 +3,10 @@ import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
 
 const border = (color='blue', size=2) => {return {borderColor:color, borderWidth:size}}
 
+
+export const SPACE_BETWEEN_SHELVES = 40
+
+
 // TODO: do something about image rendering at some point...
 const renderNew = (timeFirstRead) => {
   if (!timeFirstRead) return (
@@ -16,22 +20,33 @@ const renderNew = (timeFirstRead) => {
 const itemBuffer = 8
 const thumbHeightBump = 25
 
-export default GridItem = ({title, timeFirstRead, imageSrc, rowItemWidth, customPress, index}) => (
-  <View style={[styles.rowContainer, {height:rowItemWidth+thumbHeightBump, minWidth:rowItemWidth}]} >
-    <View  style={[styles.thumbContainer, {width:rowItemWidth-itemBuffer, height:rowItemWidth+thumbHeightBump}]} >
-      <TouchableOpacity onPress={()=>customPress(index)} >
-        <Image style={[styles.thumbnail, {minWidth:rowItemWidth-itemBuffer, minHeight:rowItemWidth+thumbHeightBump}]} source={{uri:imageSrc}} />
-        { renderNew(timeFirstRead) }
-      </TouchableOpacity>
+const renderLeft = (index, itemsPerRow) => {
+  return !(index % itemsPerRow) ? 10 : 0;
+}
+
+const renderRight = (index, itemsPerRow) => {
+  return ((index % itemsPerRow) == itemsPerRow-1) ? 30 : 0;
+}
+
+export default GridItem = ({title, timeFirstRead, imageSrc, rowItemWidth, customPress, index}) => {
+  const imgWidth = rowItemWidth*.9
+  return (
+    <View style={[styles.rowContainer, {left:renderLeft(index,2), paddingRight:renderRight(index,2), height:imgWidth+thumbHeightBump, minWidth:rowItemWidth}]} >
+      <View  style={[styles.thumbContainer, { width:imgWidth-itemBuffer, height:imgWidth+thumbHeightBump}]} >
+        <TouchableOpacity onPress={()=>customPress(index)} >
+          <Image style={[styles.thumbnail, {minWidth:imgWidth-itemBuffer, minHeight:imgWidth+thumbHeightBump}]} source={{uri:imageSrc}} />
+          { renderNew(timeFirstRead) }
+        </TouchableOpacity>
+      </View>
     </View>
-  </View>
-)
+  )
+}
 
 
 const styles = StyleSheet.create({
   rowContainer: {
     flexDirection:'column',
-    marginBottom: 40,
+    marginBottom: SPACE_BETWEEN_SHELVES,
     // borderWidth:2,
     // borderColor:'red',
   },
@@ -49,7 +64,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     width:50,
     height:50,
-    left:5,
-    bottom:-2
+    left:20,
+    bottom:20
   }
 })
